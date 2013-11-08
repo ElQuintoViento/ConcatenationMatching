a03dc4603337ec5bfcdd6dfae5d3a4da489e01f8
Adam Thorson <adamdanielthorson@gmail.com>
2013-11-08 11:52:49 -0600

    Almost done cleaning; still need to do some final checks and make able to run from command line

--- src/ConcatenationMatching.java
+++ src/ConcatenationMatching.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 
 public class ConcatenationMatching {
 
+	
+	
 	public static void setupTriesAndLists(LinkedList[] lists, Trie[] tries, LinkedList[] formable_words){
 		for(char c=97; c < 123; ++c){
 			lists[c-97] = new LinkedList();
@@ -12,6 +14,7 @@ public class ConcatenationMatching {
 			formable_words[c-97] = new LinkedList();
 		}
 	}
+	
 	
 	
 	public static void collectInput(BufferedReader buffer_reader, LinkedList[] lists) throws IOException{
@@ -27,6 +30,7 @@ public class ConcatenationMatching {
 			lists[index].add(temporary_node);
 		}
 	}
+	
 	
 	
 	public static void createTries(LinkedList[] lists, Trie[] tries){
@@ -54,6 +58,7 @@ public class ConcatenationMatching {
 	}
 	
 	
+	
 	public static void findFormableWords(LinkedList[] lists, Trie[] tries, LinkedList[] formable_words){
 		Thread[] find_threads = new Thread[26];
 		
@@ -74,21 +79,120 @@ public class ConcatenationMatching {
 	}
 	
 	
+	
+	public static int groupLargestWordOrWords(LinkedList[] formable_words, LinkedList largest_formable_words){
+		int formable_word_amount = 0;
+		int largest_length = 0;
+		
+		String temporary_word;
+		
+		WordNode next_node;
+		WordNode temporary_node;
+		
+		for(int i=0; i < 26; ++i){
+			if((temporary_node = formable_words[i].getHead()) != null){
+				formable_word_amount += formable_words[i].getSize();
+				
+				next_node = temporary_node.getNext();
+				temporary_word = temporary_node.getWord();
+			
+				// Next word is greater in size than the currently listed longest word
+				// Must become the head of the largest words
+				// Forget old largest words
+				if(temporary_word.length() >  largest_length){
+					largest_formable_words.clear();
+					largest_formable_words.add(temporary_node.copy());
+					largest_length = temporary_word.length();
+					
+					// Add other ties
+					while((next_node != temporary_node) && (next_node.getWord().length() == largest_length)){
+						largest_formable_words.add(next_node.copy());
+						next_node = next_node.getNext();
+					}
+					
+				}else if(temporary_word.length() == largest_length){
+					largest_formable_words.add(temporary_node.copy());
+					
+					// Add other ties
+					while((next_node != temporary_node) && (next_node.getWord().length() == largest_length)){
+						largest_formable_words.add(next_node.copy());
+						next_node = next_node.getNext();
+					}
+				}
+			}
+		}
+		
+		return formable_word_amount;
+	}
+	
+	
+	
+	public static void outputResults(LinkedList largest_formable_words, int formable_word_amount){
+		WordNode next_node;
+		
+		System.out.print("\nLongest Word");
+		if(largest_formable_words.getSize() > 1)
+			System.out.print("s");
+		System.out.print(":");
+		
+		if(largest_formable_words.getSize() > 0){
+			next_node = largest_formable_words.getHead();
+			
+			for(int i = 0; i < largest_formable_words.getSize(); ++i){
+				if((i % 5) == 0)
+					System.out.print("\n\t");
+				System.out.print(next_node.getWord());
+				
+				// Adding Commas
+				if(((i+1) % (largest_formable_words.getSize())) != 0)
+					System.out.print(", ");
+				
+				next_node = next_node.getNext();
+			}
+			
+			System.out.print("\n\nBesides the longest word, " + (formable_word_amount-largest_formable_words.getSize()) + " words can be formed from other words within the list.");
+			
+		}else{
+			System.out.println("\nNo words could be formed by permutations of other smaller words.\n");
+		}
+		
+		System.out.println('\n');
+	}
+	
+	
+	
+	public static void displayIOError(String io_message){
+		System.err.println("\nError: Faulty Input Was Provided Or General IO Issues Have Occurred\n" +
+				"Please Read Error Message To Have A Better Understanding Of The Error:\n"+
+				"\n\tMessage:  " + io_message + '\n');
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		
 		try(BufferedReader buffer_reader = new BufferedReader(new FileReader("C:\\Users\\Thor\\Downloads\\words.txt"))){
 			
-			//
 			int index = -1;
+			int largest_length = 0;
+			int formable_word_amount = 0;
 			
-			//
-			String temporary_string;
-			WordNode temporary_node;
-			Trie temporary_trie;
+			LinkedList largest_formable_words = new LinkedList();
 			LinkedList temporary_list;
 			LinkedList[] alphabetical_lists = new LinkedList[26];
 			LinkedList[] formable_words = new LinkedList[26];
+			
+			String first_large_word = "";
+			String temporary_string;
+			String temporary_word = "";
+			
+			
+			Trie temporary_trie;
 			Trie[] alphabetical_tries = new Trie[26];
+			
+			WordNode next_node;
+			WordNode temporary_node;
+			
 			
 			setupTriesAndLists(alphabetical_lists, alphabetical_tries, formable_words);
 			
@@ -98,81 +202,16 @@ public class ConcatenationMatching {
 			
 			findFormableWords(alphabetical_lists, alphabetical_tries, formable_words);
 			
-			LinkedList largest_formable_words = new LinkedList();
-			WordNode next_node;
-			String temporary_word = "";
-			String first_large_word = "";
-			int largest_length = 0;
-			int formable_word_amount = 0;
+			// Find the total amount of formable words from all the lists
+			formable_word_amount = groupLargestWordOrWords(formable_words, largest_formable_words);
 			
-			for(int i=0; i < 26; ++i){
-				if((temporary_node = formable_words[i].getHead()) != null){
-					formable_word_amount += formable_words[i].getSize();
-					
-					next_node = temporary_node.getNext();
-					temporary_word = temporary_node.getWord();
-				
-					// Next word is greater in size than the currently listed longest word
-					// Must become the head of the largest words
-					// Forget old largest words
-					if(temporary_word.length() >  largest_length){
-						largest_formable_words.clear();
-						largest_formable_words.add(temporary_node.copy());
-						largest_length = temporary_word.length();
-						
-						// Add other ties
-						while((next_node != temporary_node) && (next_node.getWord().length() == largest_length)){
-							largest_formable_words.add(next_node.copy());
-							next_node = next_node.getNext();
-						}
-						
-					}else if(temporary_word.length() == largest_length){
-						largest_formable_words.add(temporary_node.copy());
-						
-						// Add other ties
-						while((next_node != temporary_node) && (next_node.getWord().length() == largest_length)){
-							largest_formable_words.add(next_node.copy());
-							next_node = next_node.getNext();
-						}
-					}
-				}
-			}
+			outputResults(largest_formable_words, formable_word_amount);
 			
-			System.out.print("Longest Word");
-			if(largest_formable_words.getSize() > 1)
-				System.out.print("s");
-			System.out.print(":");
-			
-			if(largest_formable_words.getSize() > 0){
-				next_node = largest_formable_words.getHead();
-				
-				for(int i = 0; i < largest_formable_words.getSize(); ++i){
-					if((i % 5) == 0)
-						System.out.print("\n\t");
-					System.out.print(next_node.getWord());
-					
-					// Adding Commas
-					if(((i+1) % (largest_formable_words.getSize())) != 0)
-						System.out.print(", ");
-					
-					next_node = next_node.getNext();
-				}
-				
-				System.out.print("\n\nBesides the longest word, " + (formable_word_amount-largest_formable_words.getSize()) + " words can be formed from other words within the list.");
-				
-			}else{
-				System.out.println("\nNo words could be formed by permutations of other smaller words.\n");
-			}
-			
-			System.out.println('\n');
-			
-			
+		// IO Issues have occurred; possibly due to bad input.
+		// Output exception message.
 		}catch(IOException ioe){
-			System.err.println("Error: Continue to try!");
+			displayIOError(ioe.getMessage());
 		}
 
 	}
-	
-	
-	
 }
